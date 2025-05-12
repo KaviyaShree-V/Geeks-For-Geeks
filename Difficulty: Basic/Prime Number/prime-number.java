d6fddf569cb77e5b83c99faa2fb5c76dab08a943
KaviyaShree-V <karthikavi1976@gmail.com>
2025-05-12 16:40:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Prime Number/prime-number.java
+++ Difficulty: Basic/Prime Number/prime-number.java
@@ -26,19 +26,21 @@ class GFG {
 
 
 class Solution {
-    static boolean isPrime(int n) {
+    static boolean isPrime(int n) 
+    {
         // code here
-        boolean b = true;
-        if (n <= 1) {
-            b = false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    b = false;
-                    break;
+        int count=0;
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                count++;
+                if(n/i!=i){
+                    count++;
                 }
             }
         }
-    return b;
+        if(count==2){
+            return true;
+        }
+        return false;
     }
 }
