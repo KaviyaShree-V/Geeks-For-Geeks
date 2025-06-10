7cd6707fe62ce04d86c2e7890d8c4693078dbb27
KaviyaShree-V <karthikavi1976@gmail.com>
2025-06-10 15:45:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -0,0 +1,41 @@
+// User function Template for Java
+
+class Solution {
+    int search(int[] nums, int target) {
+        // Complete this function
+        int searchElement =-1;
+        int low = 0, high = nums.length - 1;
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(nums[low] > nums[mid])
+            {
+                if(nums[mid] <= target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            else
+            {
+                if(nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+        }
+        return searchElement;
+    }
+}
+  
